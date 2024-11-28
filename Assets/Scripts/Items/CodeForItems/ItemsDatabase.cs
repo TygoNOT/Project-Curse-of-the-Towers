7b96ae03da02137f91bd28ee6fe3d74320a82d6a
Nikita Proskuryakov <tygovanderm@gmail.com>
2024-11-28 19:46:15 +0100

    Bug fix

--- Assets/Scripts/Items/CodeForItems/ItemsDatabase.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "ItemDatabase", menuName = "Inventory/Item Database")]
-public class ItemDatabase : ScriptableObject
-{
-    public List<Item> items;
-}
